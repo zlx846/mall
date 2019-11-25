69e2578c93d94de06c7779413bb7f9173dbef44c
zlx846 <1275251619@qq.com>
2019-11-25 14:01:46 +0800

    a vue.js app

--- src/network/request.js
+++ src/network/request.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 export function request(config) {
   
   const instance = axios.create({
-    // baseURL: 'http://111.230.134.57:3000',
-    baseURL: 'http://192.168.1.109:3000',
+    baseURL: 'http://111.230.134.57:3000',
+    // baseURL: 'http://192.168.1.109:3000',
     timeout: 5000
   })
   return instance(config)
